ba97a8587b2b41070e4733eb1fdffcfc0a25bf31
Christopher Negron <sonycar2000@outlook.com>
2020-09-22 14:40:20 -0400

    Add files via upload

---
+++ negroncj_Assignment03/MarcoPolo.cs
@@ -0,0 +1,148 @@
+﻿/* Christopher Negron
+ * negroncj@mail.uc.edu
+ * Contemporary Programming, IT3045
+ * Assignment #3
+ * Due: 9/17/2020
+ * Design and program a class that models something in the real world. You are the domain expert: pick something that has meaning to you.
+ * 
+ * Your VS solution should have 2 classes: one will contain your entry point (Main method) and the other will contain your real-world model. 
+ * In the Main method declare and instantiate your class, then demonstrate how it  works. 
+ * 
+ * Your model should have at least:
+ * one static method, // done
+ * one static property, //done
+ * two  non-static methods, //done
+ * and three non-static properties. //done
+ * Demonstrate all these artifatcs in your Main method. // done
+ * 
+ * Refrence: 
+ * Visual C# How to program: Chapter 5.6 if else
+ * Class Meeting on 9/15/2020
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace negroncj_Assignment03
+{
+    public class MarcoPolo
+    {
+        //Properties
+        //I make it private so the user cant manipulate it.
+        private String mResponse; //Make it say marco or polo when I ask
+        private String mAction; // find out how the robot will interact. 
+        private String mFound; //If the computer found me or not.
+       
+
+        public String Response //non static propertie 1/3
+        {
+            get
+            {
+                return mResponse; //Returns the set
+            }
+
+            set
+            {
+                if (value == "marco" || value == "Marco") // if you say marco
+                {
+                    //It will say Polo
+                    mResponse = "Polo!";
+                }
+                else
+                {
+                    if (value == "polo" || value == "Polo") // If you say polo
+                    {
+                        //It will say Marco
+                        mResponse = "Marco!";
+                    }
+                }
+            }
+        }
+        public String Action //non static propertie 2/3
+        {
+            get
+            {
+                return mAction; //Returns the set
+            }
+
+            set
+            {
+                if (value == "pool" || value == "Pool") // If we are in a pool
+                {
+                    //It will swim
+                    mAction = "Action: Swimming";
+                }
+                else
+                {
+                    if (value == "Backyard" || value == "backyard") // If we are in a backyard
+                    {
+                       //It will run
+                        mAction = "Action: Running";
+                    }
+                }
+
+            }
+        }
+        public String Found //non static property 3/3
+        {
+            get
+            {
+                return mFound; //Returns the set
+            }
+            set
+            {
+                if (value == "yes" || value == "Yes") //If I say yes
+                {
+                    //He will respond
+                    mFound = "Gotcha!";
+                }
+                else
+                {
+                    if (value == "no" || value == "No") //If I say no
+                    {
+                        //he will ask
+                        mFound = "Where are you?";
+                    }
+                }
+            }
+        }
+
+        public static string Feedback //static property 1/1
+        {
+            get; //Will send back what the user put in main
+            set;
+           
+        }
+        public static string CanYouHearMeNow(String Feedback) //static method 1/1
+        {
+            //If a spesific string is said
+            if (Feedback == "Can you hear me?")
+            {
+                //It will respond
+                return "YES! I can";
+            }
+            else //But if not
+            {
+                //it will just show periods
+                return ".......";
+            }
+            
+
+        }
+        public string NamePlayer(String player) //non static method 1/2
+        {
+            //Returns the name of the player you have put in main
+            return player;
+        }
+        public string FinalResult(String WinOrLose) //non static method 2/2
+        {
+            //Will return the string you put in main
+            return WinOrLose;
+        }
+    }
+}
+
---
+++ negroncj_Assignment03/Program.cs
@@ -0,0 +1,105 @@
+﻿/* Christopher Negron
+ * negroncj@mail.uc.edu
+ * Contemporary Programming, IT3045
+ * Assignment #3
+ * Due: 9/17/2020
+ * Design and program a class that models something in the real world. You are the domain expert: pick something that has meaning to you.
+ * 
+ * Your VS solution should have 2 classes: one will contain your entry point (Main method) and the other will contain your real-world model. 
+ * In the Main method declare and instantiate your class, then demonstrate how it  works. 
+ * 
+ * Your model should have at least:
+ * one static method, // done
+ * one static property, //done
+ * two  non-static methods, //done
+ * and three non-static properties. //done 
+ * Demonstrate all these artifatcs in your Main method. //done
+ * 
+ * Refrence: 
+ * Visual C# How to program: Chapter 5.6 if else
+ * Class Meeting on 9/15/2020
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace negroncj_Assignment03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MarcoPolo marcoPolo = new MarcoPolo();
+            //I want to play a game with my program, Marco and Polo
+            // A fun game about useing your ears to find the other person
+            //to do that I am going to make a other class 
+            //Who are we playing with?
+            String Player = "Windows 10";
+            String YourName = "Christopher";
+            //Non static Method 1/2
+            marcoPolo.NamePlayer(Player);
+            Console.WriteLine("Player: "+ Player);
+
+            //Can I talk to it?
+            //static property 1/1
+            MarcoPolo.Feedback = "hello";
+            Console.WriteLine("\n" + YourName +": "+"Hello?");
+            Console.WriteLine(Player +": "+MarcoPolo.Feedback);
+
+           
+
+            //Can player hear me?
+            String result = "Can you hear me?";
+
+            //Static Method 1/1
+            MarcoPolo.CanYouHearMeNow(result);
+            Console.WriteLine("\n" + YourName +": "+result);
+            Console.WriteLine(Player + ": "+MarcoPolo.CanYouHearMeNow(result));
+
+            //I say Marco to the other class
+
+
+
+            String scream = "Marco";
+            // non-static properties 1/3
+            marcoPolo.Response = scream;
+            // Responds with polo
+            Console.WriteLine("\n" + YourName + ": " + "lets play Marco Polo");
+            Console.WriteLine(YourName + ": " + scream);
+            Console.WriteLine(Player + ": " + marcoPolo.Response);
+
+
+            //Area we are playing
+
+
+            String Area = "pool";
+            //non-static properties 2/3
+            marcoPolo.Action = Area;
+            //Responds with Action: swimming
+            Console.WriteLine("\n" + YourName + ": " + "we are playing in a " +Area);
+            Console.WriteLine(Player + ": "+marcoPolo.Action);
+
+            //did he found me?
+
+            String DidHeFindMe = "yes";
+            //non-static properties 3/3
+            marcoPolo.Found = DidHeFindMe;
+            Console.WriteLine("\n"+YourName + ": " + "Did you find me?: " + DidHeFindMe);
+            Console.WriteLine(Player + ": " + marcoPolo.Found);
+
+            
+
+            //Now lets show all the informacion we got in one sheet
+           //non static method 2/2
+           Console.WriteLine("\n"+Player +" has "+marcoPolo.FinalResult("Won"));
+           Console.WriteLine("\n" + YourName + " has " + marcoPolo.FinalResult("Lose"));
+
+        }
+        
+    }
+}
